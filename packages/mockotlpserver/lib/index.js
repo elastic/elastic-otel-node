a0cab6cf5439fd9250270b48d6261af4dc4ded1f
Trent Mick <trent.mick@elastic.co>
2023-12-20 17:52:42 -0800

    feat(mockotlpserver): use diagnostic channels for passing parsed OTLP data to printers
    The HTTP and gRPC servers will now publish parsed OTLP data to
    diagnostics channels named 'otlp.v1.trace', etc.
    Then there is a simple Printer class which supports a subclass
    with 'printTrace' et al methods. Those will subscribe to the
    'otlp.*' channels and print as needed.
    
    Multiple independent subscribers can be added. We can change
    the 'Printer' name later if something else make better sense.
    
    Using diagnostics_channel required bumping the min node to 14.17.0.
    
    The only current printer is the InspectPrinter.

--- packages/mockotlpserver/lib/index.js
+++ packages/mockotlpserver/lib/index.js
@@ -1,7 +1,7 @@
 const luggite = require('./luggite');
-
 const {startHttp} = require('./http');
 const {startGrpc} = require('./grpc');
+const {InspectPrinter} = require('./printers');
 
 const log = luggite.createLogger({name: 'mockotlpserver'});
 
@@ -12,7 +12,8 @@ const DEFAULT_HOSTNAME = 'localhost';
 const DEFAULT_HTTP_PORT = 4318;
 const DEFAULT_GRPC_PORT = 4317;
 
-// Start a server which accepts incoming HTTP requests. Exporters supported:
+// Start a server which accepts incoming OTLP/HTTP calls and publishes
+// received request data to the `otlp.*` diagnostic channels.
 // Handles `OTEL_EXPORTER_OTLP_PROTOCOL=http/proto` and
 // `OTEL_EXPORTER_OTLP_PROTOCOL=http/json`.
 startHttp({
@@ -21,12 +22,15 @@ startHttp({
     port: DEFAULT_HTTP_PORT,
 });
 
-// Start a server which accepts incoming GRPC calls.
+// Start a server which accepts incoming OTLP/gRPC calls and publishes
+// received request data to the `otlp.*` diagnostic channels.
 // Handles `OTEL_EXPORTER_OTLP_PROTOCOL=grpc`.
-//
 // NOTE: to debug read this: https://github.com/grpc/grpc-node/blob/master/TROUBLESHOOTING.md
 startGrpc({
     log,
     hostname: DEFAULT_HOSTNAME,
     port: DEFAULT_GRPC_PORT,
 });
+
+const printer = new InspectPrinter();
+printer.subscribe();
