9c43d3f8d230087abc0bbe1bcbdee9f4858b82b0
Trent Mick <trent.mick@elastic.co>
2024-01-23 09:03:36 -0800

    refactor: move logging setup to separate logging.js

---
+++ packages/opentelemetry-node/lib/logging.js
@@ -0,0 +1,64 @@
+const {api} = require('@opentelemetry/sdk-node');
+
+const luggite = require('./luggite');
+
+/**
+ * This is passed the user-provided value of `OTEL_LOG_LEVEL`. It should return
+ * null if the value is unrecognized.
+ *
+ * Dev Note: `OTEL_LOG_LEVEL`s are not standardized.
+ * https://github.com/open-telemetry/opentelemetry-specification/issues/920
+ * https://github.com/open-telemetry/opentelemetry-specification/issues/2039
+ */
+function luggiteLevelFromOtelLogLevel(otelLogLevel) {
+    const luggiteLevel =
+        {
+            NONE: luggite.FATAL + 1, // TODO: support 'silent' luggite level
+            ERROR: 'error',
+            WARN: 'warn',
+            INFO: 'info',
+            DEBUG: 'debug',
+            VERBOSE: 'trace',
+            ALL: 'trace',
+        }[otelLogLevel] || null;
+    return luggiteLevel;
+}
+
+/**
+ * Create a logger using the level from OTEL_LOG_LEVEL, default 'info'.
+ * Also set this logger to handle `api.diag.*()` log methods.
+ */
+function createLogger() {
+    let level;
+    let diagLevel;
+    if (process.env.OTEL_LOG_LEVEL) {
+        const otelLogLevel = process.env.OTEL_LOG_LEVEL.toUpperCase();
+        level = luggiteLevelFromOtelLogLevel(otelLogLevel);
+        diagLevel = otelLogLevel;
+        // Make sure NodeSDK doesn't see this envvar and overwrite our diag logger.
+        delete process.env.OTEL_LOG_LEVEL;
+    }
+    if (!level) {
+        level = 'info'; // default level
+        diagLevel = 'INFO';
+    }
+
+    const log = luggite.createLogger({name: 'elastic-otel-node', level});
+    // TODO: when luggite supports .child, add a module/component attr for diag log output
+    api.diag.setLogger(
+        {
+            error: log.error.bind(log),
+            warn: log.warn.bind(log),
+            info: log.info.bind(log),
+            debug: log.debug.bind(log),
+            verbose: log.trace.bind(log),
+        },
+        api.DiagLogLevel[diagLevel]
+    );
+
+    return log;
+}
+
+module.exports = {
+    createLogger,
+};
