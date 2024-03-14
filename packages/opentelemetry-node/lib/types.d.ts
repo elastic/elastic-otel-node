f16a492a345e3ff2389013dd5d9c19eba31f8956
David Luna <luna.bistuer@gmail.com>
2024-03-14 10:46:07 +0100

    chore: export types

--- packages/opentelemetry-node/lib/types.d.ts
+++ packages/opentelemetry-node/lib/types.d.ts
@@ -1,3 +1,3 @@
 // TODO: check if we need to reexport types from instrumentations
-export type {getInstrumentations} from './instrumentations';
-export type {ElasticNodeSDK} from './sdk';
+export {getInstrumentations} from './instrumentations';
+export {ElasticNodeSDK} from './sdk';
