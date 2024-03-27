735d757b5651a0be9b9ba4722a2638e4a587706f
David Luna <david.luna@elastic.co>
2024-03-27 18:28:41 +0100

    feat(opentelemetry-node): instrumentation-fastify (#104)

---
+++ packages/opentelemetry-node/test/fixtures/use-fastify.js
@@ -0,0 +1,37 @@
+// Usage: node -r ../../start.js use-fastify.js
+
+const http = require('http');
+
+const fastify = require('fastify');
+
+const server = fastify();
+server.get('/ping', function (req, reply) {
+    reply.send('pong');
+});
+
+server.get('/hi/:name', function (req, reply) {
+    reply.send(`Hi, ${req.params?.name || 'buddy'}.`);
+});
+
+async function main() {
+    await server.listen({port: 3000});
+
+    const port = server.server.address().port;
+
+    await new Promise((resolve) => {
+        http.get(`http://localhost:${port}/ping`, (res) => {
+            res.resume();
+            res.on('end', resolve);
+        });
+    });
+    await new Promise((resolve) => {
+        http.get(`http://localhost:${port}/hi/Bob`, (res) => {
+            res.resume();
+            res.on('end', resolve);
+        });
+    });
+
+    server.close();
+}
+
+main();
---
+++ packages/opentelemetry-node/test/instr-fastify.test.js
@@ -0,0 +1,57 @@
+// Test that 'fastify' instrumentation generates the telemetry we expect.
+
+const test = require('tape');
+const {runTestFixtures, findObjInArray} = require('./testutils');
+
+/** @type {import('./testutils').TestFixture[]} */
+const testFixtures = [
+    {
+        name: 'use-fastify',
+        args: ['./fixtures/use-fastify.js'],
+        cwd: __dirname,
+        env: {
+            NODE_OPTIONS: '--require=../start.js',
+        },
+        versionRanges: {
+            node: '>=14.18.0',
+        },
+        // verbose: true,
+        checkTelemetry: (t, col) => {
+            // We expect spans like this
+            // ------ trace 9042af (3 spans) ------
+            //         span 5894e1 "GET" (6.8ms, SPAN_KIND_CLIENT, GET http://localhost:3000/ping -> 200)
+            //     +3ms `- span c793a1 "GET /ping" (2.7ms, SPAN_KIND_SERVER, GET http://localhost:3000/ping -> 200)
+            //     +1ms   `- span f16000 "request handler - fastify" (1.0ms, SPAN_KIND_INTERNAL)
+            // ------ trace 8f3ed8 (3 spans) ------
+            //         span 6e0fc9 "GET" (1.6ms, SPAN_KIND_CLIENT, GET http://localhost:3000/hi/Bob -> 200)
+            //     +1ms `- span 40c4a8 "GET /hi/:name" (0.3ms, SPAN_KIND_SERVER, GET http://localhost:3000/hi/Bob -> 200)
+            //     +0ms   `- span 74a68a "request handler - fastify" (0.1ms, SPAN_KIND_INTERNAL)
+            const spans = col.sortedSpans;
+            t.equal(spans.length, 6);
+
+            t.equal(spans[0].scope.name, '@opentelemetry/instrumentation-http');
+            t.equal(spans[0].name, 'GET');
+            t.equal(spans[0].kind, 'SPAN_KIND_CLIENT');
+
+            t.equal(spans[1].name, 'GET /ping');
+            t.equal(spans[1].kind, 'SPAN_KIND_SERVER');
+            t.equal(spans[1].traceId, spans[0].traceId, 'same trace');
+            t.equal(spans[1].parentSpanId, spans[0].spanId);
+
+            t.equal(
+                spans[2].scope.name,
+                '@opentelemetry/instrumentation-fastify'
+            );
+            t.equal(spans[2].name, 'request handler - fastify');
+            t.equal(spans[2].kind, 'SPAN_KIND_INTERNAL');
+
+            const span = findObjInArray(spans, 'name', 'GET /hi/:name');
+            t.ok(span); // route with param
+        },
+    },
+];
+
+test('fastify instrumentation', (suite) => {
+    runTestFixtures(suite, testFixtures);
+    suite.end();
+});
