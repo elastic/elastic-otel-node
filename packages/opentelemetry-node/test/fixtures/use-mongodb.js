3f8cd7e37ca5ece79708144297986d62266c425a
David Luna <luna.bistuer@gmail.com>
2024-04-02 13:43:39 +0200

    feat: add mongodb instrumentation

---
+++ packages/opentelemetry-node/test/fixtures/use-mongodb.js
@@ -0,0 +1,35 @@
+// Usage: node -r ../../start.js use-mongodb.js
+
+const otel = require('@opentelemetry/api');
+const {MongoClient} = require('mongodb');
+
+const host = process.env.MONGODB_HOST || '127.0.0.1';
+const port = process.env.MONGODB_PORT || '27017';
+
+async function main() {
+    const url = `mongodb://${host}:${port}`;
+    const mongodbClient = new MongoClient(url);
+
+    await mongodbClient.connect();
+
+    const database = mongodbClient.db('test-db');
+    const collection = database.collection('test-col');
+
+    // https://mongodb.github.io/node-mongodb-native/4.7/classes/Collection.html#insertMany
+    let data = await collection.insertMany([{a: 1}, {a: 2}, {a: 3}], {
+        w: 1,
+    });
+    console.log('Collection insert', data);
+
+    // https://mongodb.github.io/node-mongodb-native/4.7/classes/Collection.html#deleteMany
+    data = await collection.deleteMany({a: {$lte: 3}}, {w: 1});
+    console.log('Collection delete', data);
+
+    await mongodbClient.close();
+}
+
+const tracer = otel.trace.getTracer('test');
+tracer.startActiveSpan('manual-parent-span', (span) => {
+    main();
+    span.end();
+});
