d258aba8cde51e07f976d40a879150bb43b04783
David Luna <luna.bistuer@gmail.com>
2024-04-10 17:52:58 +0200

    chore: use TSC to emit export types

--- packages/opentelemetry-node/lib/index.js
+++ packages/opentelemetry-node/lib/index.js
@@ -19,10 +19,12 @@
 
 const {ElasticNodeSDK} = require('./sdk');
 const {getInstrumentations} = require('./instrumentations');
+const {createLogger} = require('./luggite');
 
 // TODO: this should reexport things from @otel/sdk-node (like 'api', 'core', etc.)
 
 module.exports = {
     ElasticNodeSDK,
     getInstrumentations,
+    createLogger,
 };
---
+++ packages/opentelemetry-node/types/index.d.ts
@@ -0,0 +1,4 @@
+import { ElasticNodeSDK } from "./sdk";
+import { getInstrumentations } from "./instrumentations";
+import { createLogger } from "./luggite";
+export { ElasticNodeSDK, getInstrumentations, createLogger };
