99bd15ed72a2dda25b7e52f6f284dab5cc7daaee
David Luna <luna.bistuer@gmail.com>
2024-04-16 15:55:54 +0200

    chore(otel-node): update types

--- packages/opentelemetry-node/types/metrics/host.d.ts
+++ packages/opentelemetry-node/types/metrics/host.d.ts
@@ -1,17 +1,3 @@
-export type HrTime = import('@opentelemetry/api').HrTime;
-/**
- * <T>
- */
-export type Aggregator<T> = import('@opentelemetry/sdk-metrics/build/src/aggregator/types').Aggregator<T>;
-/**
- * <T>
- */
-export type AccumulationRecord<T> = import('@opentelemetry/sdk-metrics/build/src/aggregator/types').AccumulationRecord<T>;
-export type LastValueAggregation = import('@opentelemetry/sdk-metrics').LastValueAggregation;
-export type Accumulation = import('@opentelemetry/sdk-metrics/build/src/aggregator/types').Accumulation;
-export type MetricDescriptor = import('@opentelemetry/sdk-metrics').MetricDescriptor;
-export type AggregationTemporality = import('@opentelemetry/sdk-metrics').AggregationTemporality;
-export type GaugeMetricData = import('@opentelemetry/sdk-metrics').GaugeMetricData;
 /** @type {metrics.View[]} */
 export const HOST_METRICS_VIEWS: metrics.View[];
 export function enableHostMetrics(): void;
