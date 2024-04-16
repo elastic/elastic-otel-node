7136978b68692fcb3d98eb65e4f0e8e93ee2f9b9
Trent Mick <trent.mick@elastic.co>
2024-04-15 17:13:27 -0700

    chore: release process and tooling for otel-node package
    - release.yml workflow that triggers on a pushed v1.2.3 tag
    - it will create a GitHub release with changelog content
      extracted from CHANGELOG.md
    - it will npm publish with provenance
    - adds a lint:changelog script to ensure the changelog meets
      the requirements for that changelog extraction

---
+++ packages/opentelemetry-node/CHANGELOG.md
@@ -0,0 +1,10 @@
+# @elastic/opentelemetry-node Changelog
+
+## Unreleased
+
+(nothing yet)
+
+## v0.1.0
+
+The first release of the Elastic OpenTelemetry Node.js distribution.
+See [the README](https://github.com/elastic/elastic-otel-node/tree/main/packages/opentelemetry-node#readme).
---
+++ scripts/github-release.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# Create a GitHub release. If the given tag name is the *latest* version, and
+# is not a pre-release, then the GH release will be marked as the latest.
+# (This is typically only run from the release.yml CI workflow.)
+#
+# Usage:
+#   ./scripts/github-release.sh PKG_DIR TAG_NAME
+# Example:
+#   ./scripts/github-release.sh packages/opentelemetry-node v0.1.0
+#
+# - For auth, this expects the 'GH_TOKEN' envvar to have been set.
+# - The 'TAG_NAME' is typically from the 'GITHUB_REF_NAME' variable
+#   (https://docs.github.com/en/actions/learn-github-actions/variables)
+#   from a GitHub Actions workflow run.
+
+if [ "$TRACE" != "" ]; then
+    export PS4='${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+    set -o xtrace
+fi
+set -euo pipefail
+
+function fatal {
+    echo "$(basename $0): error: $*" >&2
+    exit 1
+}
+
+readonly PKG_DIR="$1"
+readonly TAG_NAME="$2"
+
+TOP=$(cd $(dirname $0)/../ >/dev/null; pwd)
+JSON=$TOP/node_modules/.bin/json
+
+if [[ ! -f "$PKG_DIR/package.json" ]]; then
+  fatal "invalid PKG_DIR arg: '$PKG_DIR/package.json' does not exist"
+fi
+if [[ -z $(git tag -l "${TAG_NAME}") ]]; then
+  fatal "invalid TAG_NAME arg: '$TAG_NAME' git tag does not exist"
+fi
+
+PKG_NAME=$($JSON -f "$PKG_DIR/package.json" name)
+PKG_VER=$($JSON -f "$PKG_DIR/package.json" version)
+if [[ "v$PKG_VER" != "$TAG_NAME" ]]; then
+  fatal "TAG_NAME, '$TAG_NAME', does not match version in package.json, '$PKG_VER'"
+fi
+
+# Extract the changelog section for this version.
+$TOP/scripts/extract-release-notes.sh $PKG_DIR
+echo "INFO: Extracted changelog"
+echo "--"
+cat build/release-notes.md
+echo "--"
+
+echo
+echo "INFO: List current GitHub releases"
+gh release list
+
+# The latest (by semver version ordering) git version tag, excluding pre-releases.
+# TODO: this assumes a tag "vN.M.O", which won't work for prefixed-tags.
+LATEST_GIT_TAG=$(git tag --list --sort=version:refname "v*" | (grep -v - || true) | tail -n1)
+if [[ -z "$LATEST_GIT_TAG" || "$TAG_NAME" == "$LATEST_GIT_TAG" ]]; then
+  IS_LATEST=true
+else
+  IS_LATEST=false
+fi
+
+echo
+echo "INFO: Creating '$PKG_NAME $TAG_NAME' GitHub release (latest=$IS_LATEST)"
+echo XXX gh release create "$TAG_NAME" \
+  --title "$PKG_NAME $PKG_VER" \
+  --notes-file build/release-notes.md \
+  --latest=$IS_LATEST
