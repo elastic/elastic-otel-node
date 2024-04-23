2a85f973795d22d851e466623a351e75d924a6ce
Trent Mick <trent.mick@elastic.co>
2024-04-23 13:16:14 -0700

    types update for logging.js changes

--- packages/opentelemetry-node/types/logging.d.ts
+++ packages/opentelemetry-node/types/logging.d.ts
@@ -1,29 +1,5 @@
-/// <reference types="node" />
+export const log: any;
 /**
- * Create a logger using the level from OTEL_LOG_LEVEL, default 'info'.
- * Also set this logger to handle `api.diag.*()` log methods.
+ * Register the singleton `log` to handle OTel `api.diag.*()` calls.
  */
-export function setupLogger(): {
-    _level: number;
-    _stringify: typeof import("safe-stable-stringify").stringify;
-    _serializers: {
-        err: (err: any) => any;
-    };
-    _haveNonRawStreams: boolean;
-    _streams: any[];
-    _fields: Record<string, any>;
-    _addStream(s: {
-        type?: string;
-        level?: string | number;
-        stream?: import("stream").Writable;
-    }, defaultLevel?: string | number): void;
-    level(value?: string | number): number;
-    _applySerializers(fields: Record<string, any>, excludeFields: Record<string, boolean>): void;
-    _emit(rec: any): void;
-    trace: (arg0: string | Record<string, any>, ...arg1: any[]) => void;
-    debug: (arg0: string | Record<string, any>, ...arg1: any[]) => void;
-    info: (arg0: string | Record<string, any>, ...arg1: any[]) => void;
-    warn: (arg0: string | Record<string, any>, ...arg1: any[]) => void;
-    error: (arg0: string | Record<string, any>, ...arg1: any[]) => void;
-    fatal: (arg0: string | Record<string, any>, ...arg1: any[]) => void;
-};
+export function registerOTelDiagLogger(api: any): void;
