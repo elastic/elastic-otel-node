00e0b8421f61c67b8f02e4ec7f09a8d15980a89a
David Luna <luna.bistuer@gmail.com>
2024-04-25 14:50:51 +0200

    chore: add dependency-check parser for mjs files

---
+++ dev-utils/mjs-detective.js
@@ -0,0 +1,44 @@
+const acorn = require('acorn-node');
+const walk = require('acorn-node/walk');
+
+function find(source, opts) {
+    const ast = acorn.parse(source, {
+        // to parse ESM
+        sourceType: 'module',
+        // top level await
+        allowAwaitOutsideFunction: true
+    });
+
+    const modules = [];
+
+    // TODO: walk the AST and return the list of modues in
+    // `strings` property
+    walk.recursive(ast, null, {
+        // import { a, b, c } from 'module';
+        ImportDeclaration (node) {
+            modules.push(node.source.value);
+        },
+        // export { a, b, c } from 'module';
+        ExportNamedDeclaration (node) {
+            modules.push(node.source.value);
+        },
+        // const x = await import('module');
+        ExpressionStatement (node) {
+            if (
+                node.expression.type === 'AwaitExpression' &&
+                node.expression.argument.type === 'CallExpression' &&
+                node.expression.argument.callee.type === 'Import'
+            ) {
+                modules.push(node.expression.argument.arguments[0].value)
+            }
+        }
+        
+    });
+    
+    // Cleanup `node:` prefix for built-in modules
+    return modules.map((m) => m.replace(/^node:/, ''));
+}
+
+module.exports = function (src, opts) {
+    return find(src, opts);
+};
