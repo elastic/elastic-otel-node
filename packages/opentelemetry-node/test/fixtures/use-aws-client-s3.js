f8757d373e680447993f373b4e24ad3ba9b76933
David Luna <luna.bistuer@gmail.com>
2024-05-08 19:47:14 +0200

    feat(instr-aws-sdk): add instrumentation and tests for AWS S3

---
+++ packages/opentelemetry-node/test/fixtures/use-aws-client-s3.js
@@ -0,0 +1,91 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Usage: node -r @elastic/opentelemetry-node use-aws-client-s3.js
+
+// This script can also be used for manual testing of APM instrumentation of S3
+// against a real S3 account. This can be useful because tests are done against
+// a local HTTP server that *simulates* S3 with imperfect fidelity.
+//
+// Auth note: By default this uses the AWS profile/configuration from the
+// environment. If you do not have that configured (i.e. do not have
+// "~/.aws/...") files, then you can still use localstack via setting:
+//    unset AWS_PROFILE
+//    export AWS_ACCESS_KEY_ID=fake
+//    export AWS_SECRET_ACCESS_KEY=fake
+// See also: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html
+//
+// Usage:
+//    # Run against the default configured AWS profile, listing all available buckets.
+//    node use-client-s3.js
+//    # Run against a local server which returns a predefined list of buckets.
+//    TEST_LOCAL=true node use-client-s3.js
+
+
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const otel = require('@opentelemetry/api');
+const {
+    S3Client,
+    ListBucketsCommand,
+} = require('@aws-sdk/client-s3');
+
+const useLocalServer = process.env.TEST_LOCAL === 'true';
+const assetsPath = path.resolve(__dirname, '../assets');
+/** @type {import('http').Server | undefined} */
+let server;
+
+async function main() {
+    const localEndpoint = 'http://localhost:4566'
+    const region = process.env.TEST_REGION || 'us-east-2';
+    const endpoint = useLocalServer ? localEndpoint : null;
+    const s3Client = new S3Client({
+        region,
+        endpoint,
+    });
+
+    const command = new ListBucketsCommand({});
+    const data = await s3Client.send(command);
+    console.log({ data }, 'listBuckets');
+}
+
+// Start mock server if required
+if (useLocalServer) {
+    server = http.createServer((req,res) => {
+        // GET /?x-id=ListBuckets
+        if (req.method === 'GET' && req.url === '/?x-id=ListBuckets') {
+            res.writeHead(200, {'Content-Type': 'application/xml'});
+            fs.createReadStream(`${assetsPath}/aws-s3-list-buckets.xml`).pipe(res);
+            return;
+        }
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('Error 404: Not Found.');
+        res.end();
+    });
+
+    server.listen(4566, 'localhost')
+}
+
+const tracer = otel.trace.getTracer('test');
+tracer.startActiveSpan('manual-parent-span', async (span) => {
+    await main();
+    span.end();
+    server?.close();
+});
---
+++ packages/opentelemetry-node/test/instr-aws.test.js
@@ -0,0 +1,76 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Test that 'aws-sdk' instrumentation generates the telemetry we expect.
+
+const test = require('tape');
+const {runTestFixtures, findObjInArray} = require('./testutils');
+
+/** @type {import('./testutils').TestFixture[]} */
+const testFixtures = [
+    {
+        name: 'use-aws-client-s3',
+        args: ['./fixtures/use-aws-client-s3.js'],
+        cwd: __dirname,
+        env: {
+            NODE_OPTIONS: '--require=@elastic/opentelemetry-node',
+            AWS_ACCESS_KEY_ID: 'fake',
+            AWS_SECRET_ACCESS_KEY: 'fake',
+            TEST_LOCAL: 'true',
+            TEST_REGION: 'us-east-2',
+        },
+        // verbose: true,
+        checkTelemetry: (t, col) => {
+            // We expect spans like this
+            //          span b592a3 "manual-parent-span" (26.1ms, SPAN_KIND_INTERNAL)
+            //     +4ms `- span bbe07e "S3.ListBuckets" (21.5ms, SPAN_KIND_CLIENT)
+            //    +10ms   `- span b3b885 "GET" (7.0ms, SPAN_KIND_CLIENT, GET http://localhost:4566/?x-id=ListBuckets -> 200)
+            //     +4ms     `- span 57189f "GET" (2.1ms, SPAN_KIND_SERVER, GET http://localhost:4566/?x-id=ListBuckets -> 200)
+            //
+            // last span is created when mock server process the request from the AWS sdk
+            const spans = col.sortedSpans;
+            t.equal(spans.length, 4);
+
+            t.equal(spans[1].scope.name, '@opentelemetry/instrumentation-aws-sdk');
+            t.equal(spans[1].name, 'S3.ListBuckets');
+            t.equal(spans[1].kind, 'SPAN_KIND_CLIENT');
+            t.equal(spans[1].traceId, spans[0].traceId, 'same trace');
+            t.equal(spans[1].parentSpanId, spans[0].spanId);
+            t.deepEqual(spans[1].attributes, {
+                'rpc.system': 'aws-api',
+                'rpc.method': 'ListBuckets',
+                'rpc.service': 'S3',
+                'aws.region': 'us-east-2',
+                'http.status_code': 200,
+            });
+
+            // NOTE: should we remove these assertions? aren't they testinh HTTP instr and not AWS?
+            t.equal(spans[2].scope.name, '@opentelemetry/instrumentation-http');
+            t.equal(spans[2].name, 'GET');
+            t.equal(spans[2].kind, 'SPAN_KIND_CLIENT');
+            t.equal(spans[2].traceId, spans[0].traceId, 'same trace');
+            t.equal(spans[2].parentSpanId, spans[1].spanId);
+        },
+    },
+];
+
+test('express instrumentation', (suite) => {
+    runTestFixtures(suite, testFixtures);
+    suite.end();
+});
