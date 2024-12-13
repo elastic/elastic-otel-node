c39c7672573d8329f572d63552b119d45e33b8ce
Trent Mick <trent.mick@elastic.co>
2024-12-13 12:21:22 -0800

    skip openai tests with node<18; openai itself requires node 18 or later

--- packages/opentelemetry-node/test/instr-elastic-openai.test.js
+++ packages/opentelemetry-node/test/instr-elastic-openai.test.js
@@ -19,13 +19,19 @@
 
 const http = require('http');
 const test = require('tape');
+const semver = require('semver');
 const {runTestFixtures, assertDeepMatch} = require('./testutils');
 
-const skip = process.env.TEST_GENAI_MODEL === undefined;
+let skip = process.env.TEST_GENAI_MODEL === undefined;
 if (skip) {
     console.log(
         '# SKIP elastic openai tests: TEST_GENAI_MODEL is not set (load env from test/test-services.env)'
     );
+} else {
+    skip = !semver.satisfies(process.version, '>=18');
+    if (skip) {
+        console.log('# SKIP elastic openai requires node >=18');
+    }
 }
 
 /** @type {import('./testutils').TestFixture[]} */
