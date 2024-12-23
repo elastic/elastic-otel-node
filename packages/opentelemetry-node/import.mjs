084285dd8fb0c64767e2084ae9f24e2becfe0d79
Trent Mick <trent.mick@elastic.co>
2024-12-23 07:58:23 -0800

    lint fix

--- packages/opentelemetry-node/import.mjs
+++ packages/opentelemetry-node/import.mjs
@@ -20,20 +20,20 @@
 // Register ESM hook and start the SDK.
 // This is called for `--import @elastic/opentelemetry-node`.
 
-import { register } from 'node:module';
+import {register} from 'node:module';
 import {isMainThread} from 'node:worker_threads';
 
 // TODO: @opentelemetry/instrumentation should re-export this IITM method.
 // XXX add explicit IITM dep?
 // XXX can we have a guard that there is only one install of IITM in play?
-import { createAddHookMessageChannel } from 'import-in-the-middle';
+import {createAddHookMessageChannel} from 'import-in-the-middle';
 
 import {log} from './lib/logging.js';
 
 // Note: If there are *multiple* installations of import-in-the-middle, then
 // only those instrumentations using this same one will be hooked.
-const { registerOptions, waitForAllMessagesAcknowledged } =
-  createAddHookMessageChannel();
+const {registerOptions, waitForAllMessagesAcknowledged} =
+    createAddHookMessageChannel();
 
 if (isMainThread) {
     log.trace('bootstrap-import: registering module hook');
