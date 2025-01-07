692a984530abd913f83cb10cd97cf425a559b698
Trent Mick <trent.mick@elastic.co>
2025-01-07 09:28:45 -0800

    chore: improved OpenAI + EDOT Node.js exapmle

---
+++ examples/openai/chat.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+const OpenAI = require('openai');
+
+let chatModel = process.env.CHAT_MODEL ?? 'gpt-4o-mini';
+
+async function main() {
+  const client = new OpenAI();
+
+  const messages = [
+    {
+      role: 'user',
+      content: 'Answer in up to 3 words: Which ocean contains Bouvet Island?',
+    },
+  ];
+
+  try {
+    const chatCompletion = await client.chat.completions.create({
+      model: chatModel,
+      messages: messages,
+    });
+    console.log(chatCompletion.choices[0].message.content);
+  } catch (err) {
+    console.log('chat err:', err);
+    process.exitCode = 1;
+  }
+}
+
+main();
