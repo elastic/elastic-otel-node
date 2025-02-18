df17e2d1a182883d26e48ac79fd4f41b3339d3c7
David Luna <luna.bistuer@gmail.com>
2025-02-18 13:00:34 +0100

    tests(otel-node): add test case for fastify enable

--- packages/opentelemetry-node/test/instr-fastify.test.js
+++ packages/opentelemetry-node/test/instr-fastify.test.js
@@ -29,11 +29,52 @@ const {
 /** @type {import('./testutils').TestFixture[]} */
 const testFixtures = [
     {
-        name: 'use-fastify',
+        name: 'use-fastify (default disabled)',
         args: ['./fixtures/use-fastify.js'],
         cwd: __dirname,
         env: {
             NODE_OPTIONS: '--require=@elastic/opentelemetry-node',
+        },
+        versionRanges: {
+            // Ref: https://fastify.dev/docs/latest/Guides/Migration-Guide-V5/#long-term-support-cycle
+            node: '>=20.0.0',
+        },
+        // verbose: true,
+        checkTelemetry: (t, col) => {
+            // We expect spans like this
+            // ------ trace 9042af (3 spans) ------
+            //         span 5894e1 "GET" (6.8ms, SPAN_KIND_CLIENT, GET http://localhost:3000/ping -> 200)
+            //     +3ms `- span c793a1 "GET /ping" (2.7ms, SPAN_KIND_SERVER, GET http://localhost:3000/ping -> 200)
+            // ------ trace 8f3ed8 (3 spans) ------
+            //         span 6e0fc9 "GET" (1.6ms, SPAN_KIND_CLIENT, GET http://localhost:3000/hi/Bob -> 200)
+            //     +1ms `- span 40c4a8 "GET /hi/:name" (0.3ms, SPAN_KIND_SERVER, GET http://localhost:3000/hi/Bob -> 200)
+            const spans = filterOutDnsNetSpans(col.sortedSpans);
+
+            t.equal(spans.length, 4);
+            t.ok(
+                spans.every(
+                    (s) =>
+                        s.scope.name === '@opentelemetry/instrumentation-http'
+                )
+            );
+            t.ok(spans.every((s) => s.name === 'GET'));
+            t.equal(
+                spans.filter((s) => s.kind === 'SPAN_KIND_CLIENT').length,
+                2
+            );
+            t.equal(
+                spans.filter((s) => s.kind === 'SPAN_KIND_SERVER').length,
+                2
+            );
+        },
+    },
+    {
+        name: 'use-fastify (enabled via env var)',
+        args: ['./fixtures/use-fastify.js'],
+        cwd: __dirname,
+        env: {
+            NODE_OPTIONS: '--require=@elastic/opentelemetry-node',
+            OTEL_NODE_ENABLED_INSTRUMENTATIONS: 'http,fastify',
         },
         versionRanges: {
             // Ref: https://fastify.dev/docs/latest/Guides/Migration-Guide-V5/#long-term-support-cycle
