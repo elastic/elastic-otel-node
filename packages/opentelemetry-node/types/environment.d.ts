a9cf349e5d76bb577e1b7706d2551e1acb1325d9
David Luna <luna.bistuer@gmail.com>
2025-03-18 17:41:27 +0100

    chore: update types

--- packages/opentelemetry-node/types/environment.d.ts
+++ packages/opentelemetry-node/types/environment.d.ts
@@ -9,3 +9,7 @@ export function setupEnvironment(): void;
  * Restores any value stashed in the setup process
  */
 export function restoreEnvironment(): void;
+export function getEnvBoolean(name: string, defaultValue?: boolean): boolean;
+export function getEnvNumber(name: string, defaultValue?: number): number;
+export function getEnvString(name: string, defaultValue?: string): string;
+export function getEnvStringList(name: string, defaultValue?: string[]): string[];
