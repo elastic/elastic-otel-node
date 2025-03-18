0edea7d9e4819dca7658ccab97a8acb70a35f8fd
Trent Mick <trent.mick@elastic.co>
2025-03-18 16:16:14 -0700

    update types

--- packages/opentelemetry-node/types/environment.d.ts
+++ packages/opentelemetry-node/types/environment.d.ts
@@ -9,7 +9,3 @@ export function setupEnvironment(): void;
  * Restores any value stashed in the setup process
  */
 export function restoreEnvironment(): void;
-export function getEnvBoolean(name: string, defaultValue?: boolean): boolean;
-export function getEnvNumber(name: string, defaultValue?: number): number;
-export function getEnvString(name: string, defaultValue?: string): string;
-export function getEnvStringList(name: string, defaultValue?: string[]): string[];
