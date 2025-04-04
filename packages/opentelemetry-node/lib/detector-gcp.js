62a35813dbc388f539b1c24f9469a6cfc1f35349
David Luna <luna.bistuer@gmail.com>
2025-04-04 15:00:08 +0200

    test: add gcp detector tests

--- packages/opentelemetry-node/lib/detector-gcp.js
+++ packages/opentelemetry-node/lib/detector-gcp.js
@@ -7,10 +7,10 @@
  * @typedef {import('@opentelemetry/resources').ResourceDetector} ResourceDetector
  */
 
-const { URL } = require('url');
+const {URL} = require('url');
 
-const { suppressTracing } = require('@opentelemetry/core');
-const { context } = require('@opentelemetry/api');
+const {suppressTracing} = require('@opentelemetry/core');
+const {context} = require('@opentelemetry/api');
 const {
     CLOUDPROVIDERVALUES_GCP,
     SEMRESATTRS_CLOUD_ACCOUNT_ID,
@@ -37,11 +37,17 @@ const gcpDetector = {
                     return undefined;
                 }
                 return {
-                    'projectId': metadataQuery('project/project-id').catch(() => ''),
-                    'instanceId': metadataQuery('instance/id').catch(() => ''),
-                    'zoneId': metadataQuery('instance/zone').catch(() => ''),
-                    'clusterName': metadataQuery('instance/attributes/cluster-name').catch(() => ''),
-                    'hostname': metadataQuery('instance/hostname').catch(() => ''),
+                    projectId: metadataQuery('project/project-id').catch(
+                        () => ''
+                    ),
+                    instanceId: metadataQuery('instance/id').catch(() => ''),
+                    zoneId: metadataQuery('instance/zone').catch(() => ''),
+                    clusterName: metadataQuery(
+                        'instance/attributes/cluster-name'
+                    ).catch(() => ''),
+                    hostname: metadataQuery('instance/hostname').catch(
+                        () => ''
+                    ),
                 };
             }
         );
@@ -53,12 +59,8 @@ const gcpDetector = {
             [SEMRESATTRS_CLOUD_ACCOUNT_ID]: metadataPromise.then(
                 (md) => md?.projectId
             ),
-            [SEMRESATTRS_HOST_ID]: metadataPromise.then(
-                (md) => md?.instanceId
-            ),
-            [SEMRESATTRS_HOST_NAME]: metadataPromise.then(
-                (md) => md?.hostname
-            ),
+            [SEMRESATTRS_HOST_ID]: metadataPromise.then((md) => md?.instanceId),
+            [SEMRESATTRS_HOST_NAME]: metadataPromise.then((md) => md?.hostname),
             [SEMRESATTRS_CLOUD_AVAILABILITY_ZONE]: metadataPromise.then(
                 (md) => md?.zoneId
             ),
@@ -80,7 +82,7 @@ const gcpDetector = {
                 (md) => md && process.env.CONTAINER_NAME
             );
         }
-        return { attributes };
+        return {attributes};
     },
 };
 
@@ -88,38 +90,47 @@ const gcpDetector = {
  * @returns {Promise<boolean>}
  */
 function isAvailable() {
-    return metadataQuery('instance').then(() => true).catch(() => false);
+    return metadataQuery('instance')
+        .then(() => true)
+        .catch(() => false);
 }
 
 /**
  * Returns the metadata from a given path. Unlike `gcp-metadata` this method
- * does not parse with `json-bigint` since the keys accessed 
- * @param {string} path 
+ * does not parse with `json-bigint` since the keys accessed
+ * @param {string} path
  * @returns {Promise<string>}
  */
 function metadataQuery(path) {
-    const baseUrl = new URL('/', 'http://metadata.google.internal');
+    const baseUrl = 'http://metadata.google.internal:80';
     const options = {
         method: 'GET',
-        headers: { 'Metadata-Flavor': 'Google' },
+        headers: {'Metadata-Flavor': 'Google'},
         signal: AbortSignal.timeout(1000),
     };
-    return fetch(`${baseUrl}/computeMetadata/v1/${path}`, options)
-        .then((res) => {
+    return fetch(`${baseUrl}/computeMetadata/v1/${path}`, options).then(
+        (res) => {
             // Validate status
             if (!(res.status >= 200 && res.status < 300)) {
-                throw new Error(`Invalid response from metadata service: invalid status code: ${res.status} text: ${res.statusText}`);
+                throw new Error(
+                    `Invalid response from metadata service: invalid status code: ${res.status} text: ${res.statusText}`
+                );
             }
             // Validation from gcp-metadata
             // https://github.com/googleapis/gcp-metadata/blob/d8a868e5f487dcc3dd4bfd2d59d8c331fcf2895b/src/index.ts#L177
             const headerVal = res.headers.get('metadata-flavor');
-            if (res.headers.get('metadata-flavor') !== 'Google') {
-                throw new Error(`Invalid response from metadata service: incorrect 'Metadata-Flavor' header. Expected 'Google', got ${headerVal || 'no header'}`);
+            if (res.headers.get('Metadata-Flavor') !== 'Google') {
+                throw new Error(
+                    `Invalid response from metadata service: incorrect 'Metadata-Flavor' header. Expected 'Google', got ${
+                        headerVal || 'no header'
+                    }`
+                );
             }
             return res.text();
-        });
+        }
+    );
 }
 
 module.exports = {
-    gcpDetector
+    gcpDetector,
 };
