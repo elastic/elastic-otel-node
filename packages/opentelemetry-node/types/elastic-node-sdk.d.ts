6a959393257eda14c4485852085b342448ffcaaf
Trent Mick <trent.mick@elastic.co>
2025-04-07 15:35:41 -0700

    drop ElasticNodeSDK subclass, no longer using it

--- packages/opentelemetry-node/types/elastic-node-sdk.d.ts
+++
@@ -1,8 +0,0 @@
-export type NodeSDKConfiguration = import('@opentelemetry/sdk-node').NodeSDKConfiguration;
-export class ElasticNodeSDK extends NodeSDK {
-    /** @private */
-    private _metricsDisabled;
-    /** @private */
-    private _log;
-}
-import { NodeSDK } from "@opentelemetry/sdk-node";
