9117e8bd3ec71d05bdd1a9a1eb5bebee8db1891e
Trent Mick <trent.mick@elastic.co>
2025-06-05 12:25:20 -0700

    feat(opamp-client-node): a Node.js OpAMP client, and mockopampserver for dev/testing (#740)
    '@elastic/opamp-client-node' and '@elastic/mockopampserver'

---
+++ packages/mockopampserver/lib/logging.js
@@ -0,0 +1,71 @@
+/*
+ * Copyright Elasticsearch B.V. and contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// This provides a `log` singleton Logger instance for this package.
+
+const util = require('util');
+
+const bunyan = require('bunyan');
+
+const _globalThis = typeof globalThis === 'object' ? globalThis : global;
+const _symLog = Symbol.for('mockopampserver.log');
+
+function createLogger() {
+    return bunyan.createLogger({
+        name: 'mockopampserver',
+        serializers: {
+            err: bunyan.stdSerializers.err,
+            req: function (req) {
+                if (!req || !req.connection) return req;
+                return {
+                    method: req.method,
+                    // Accept `req.originalUrl` for expressjs usage.
+                    // https://expressjs.com/en/api.html#req.originalUrl
+                    url: req.originalUrl || req.url,
+                    headers: req.headers,
+                    body:
+                        req.body &&
+                        util.inspect(req.body, {depth: 50, colors: true}),
+                };
+            },
+            res: function (res) {
+                if (!res || !res.statusCode) {
+                    return res;
+                }
+                return {
+                    statusCode: res.statusCode,
+                    header: res._header,
+                    body:
+                        res.body &&
+                        util.inspect(res.body, {depth: 50, colors: true}),
+                };
+            },
+            agent: function (agent) {
+                // `agent` is the data the mockopapmserver keeps on reporting
+                // OpAMP clients. See `_processAgentToServer`. The main issue
+                // is that it holds some BigInts, which JSON serializing balks
+                // on.
+                return util.inspect(agent, {depth: 50, colors: true});
+            },
+        },
+        level: 'info',
+        stream: process.stdout,
+    });
+}
+
+// ---- main line
+
+// Create, if necessary, and export a singleton `log` logger instance.
+if (_globalThis[_symLog] === undefined) {
+    _globalThis[_symLog] = createLogger();
+}
+/** @type {import('./bunyan').Logger} */
+const log = _globalThis[_symLog];
+
+// ---- exports
+
+module.exports = {
+    log,
+};
