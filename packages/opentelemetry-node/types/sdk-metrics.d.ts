35bcb2b2463715149a657d94e7993debaa1dcfcc
David Luna <luna.bistuer@gmail.com>
2025-06-26 09:46:12 +0200

    chore: update types

---
+++ packages/opentelemetry-node/types/sdk-metrics.d.ts
@@ -0,0 +1,10 @@
+export type Meter = import('@opentelemetry/api').Meter;
+export type UpDownCounter = import('@opentelemetry/api').UpDownCounter;
+export type Counter = import('@opentelemetry/api').Counter;
+export type SpanProcessor = import('@opentelemetry/sdk-trace-base').SpanProcessor;
+/**
+ *
+ * @param {Partial<import('@opentelemetry/sdk-node').NodeSDKConfiguration>} cfg
+ * @returns
+ */
+export function setupSdkMetrics(cfg: Partial<import('@opentelemetry/sdk-node').NodeSDKConfiguration>): void;
--- packages/opentelemetry-node/types/semconv.d.ts
+++ packages/opentelemetry-node/types/semconv.d.ts
@@ -24,3 +24,39 @@ export const ATTR_DEPLOYMENT_ENVIRONMENT_NAME: "deployment.environment.name";
  * @experimental This attribute is experimental and is subject to breaking changes in minor releases of `@opentelemetry/semantic-conventions`.
  */
 export const ATTR_DEPLOYMENT_NAME: "deployment.name";
+/**
+ * The number of created spans for which the end operation was called
+ *
+ * @note For spans with `recording=true`: Implementations **MUST** record both `otel.sdk.span.live` and `otel.sdk.span.ended`.
+ * For spans with `recording=false`: If implementations decide to record this metric, they **MUST** also record `otel.sdk.span.live`.
+ *
+ * @experimental This metric is experimental and is subject to breaking changes in minor releases of `@opentelemetry/semantic-conventions`.
+ */
+export const METRIC_OTEL_SDK_SPAN_ENDED: "otel.sdk.span.ended";
+/**
+ * The number of created spans for which the end operation has not been called yet
+ *
+ * @note For spans with `recording=true`: Implementations **MUST** record both `otel.sdk.span.live` and `otel.sdk.span.ended`.
+ * For spans with `recording=false`: If implementations decide to record this metric, they **MUST** also record `otel.sdk.span.ended`.
+ *
+ * @experimental This metric is experimental and is subject to breaking changes in minor releases of `@opentelemetry/semantic-conventions`.
+ */
+export const METRIC_OTEL_SDK_SPAN_LIVE: "otel.sdk.span.live";
+/**
+ * The result value of the sampler for this span
+ *
+ * @experimental This attribute is experimental and is subject to breaking changes in minor releases of `@opentelemetry/semantic-conventions`.
+ */
+export const ATTR_OTEL_SPAN_SAMPLING_RESULT: "otel.span.sampling_result";
+/**
+ * Enum value "DROP" for attribute {@link ATTR_OTEL_SPAN_SAMPLING_RESULT}.
+ */
+export const OTEL_SPAN_SAMPLING_RESULT_VALUE_DROP: "DROP";
+/**
+ * Enum value "RECORD_AND_SAMPLE" for attribute {@link ATTR_OTEL_SPAN_SAMPLING_RESULT}.
+ */
+export const OTEL_SPAN_SAMPLING_RESULT_VALUE_RECORD_AND_SAMPLE: "RECORD_AND_SAMPLE";
+/**
+ * Enum value "RECORD_ONLY" for attribute {@link ATTR_OTEL_SPAN_SAMPLING_RESULT}.
+ */
+export const OTEL_SPAN_SAMPLING_RESULT_VALUE_RECORD_ONLY: "RECORD_ONLY";
