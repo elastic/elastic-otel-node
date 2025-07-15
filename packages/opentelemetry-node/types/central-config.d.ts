fa245cd9f6d400d497a9891a00aa429b701ae9d0
Trent Mick <trent.mick@elastic.co>
2025-07-15 13:48:35 -0700

    update types

--- packages/opentelemetry-node/types/central-config.d.ts
+++ packages/opentelemetry-node/types/central-config.d.ts
@@ -1,6 +1,19 @@
 /**
+ * A "setter" is a function that applies one or more config keys.
+ *
+ * - A config value of `undefined` means that the setting should be reset to its default value.
+ * - After setting the value: `log.info('central-config: ...')`
+ * - If there is an error applying the value, an error message string must be returned.
+ */
+export type RemoteConfigHandler = {
+    keys: string[];
+    setter: (config: any, _sdkInfo: any) => string | null;
+};
+/**
  * Setup an OpAMP client, if configured to use one.
+ *
+ * TODO: type for sdkInfo
  *
  * @returns {object | null} OpAMPClient, if configured to use one.
  */
-export function setupCentralConfig(resource: any): object | null;
+export function setupCentralConfig(sdkInfo: any): object | null;
