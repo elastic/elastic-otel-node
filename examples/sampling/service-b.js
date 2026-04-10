8ace093310daf3c9ca1609d70382012db6e47e10
Trent Mick <trent.mick@elastic.co>
2026-04-10 09:32:14 -0700

    docs: an example showing some head-based sampling details (#1439)

---
+++ examples/sampling/service-b.js
@@ -0,0 +1,28 @@
+/*
+ * Copyright Elasticsearch B.V. and contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const http = require('http');
+
+const NAME = 'b';
+const PORT = 3301;
+
+const server = http.createServer(function onRequest(req, res) {
+    console.log(
+        '[%s] incoming request: %s %s %s',
+        NAME,
+        req.method,
+        req.url,
+        req.headers
+    );
+    req.resume();
+    req.on('end', function () {
+        res.writeHead(200);
+        res.end();
+    });
+});
+
+server.listen(PORT, '127.0.0.1', async function () {
+    console.log('[%s] listening:', NAME, server.address());
+});
